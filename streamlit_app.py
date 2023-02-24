9a6ea6d7a032fc46f9fa91384df475f19b69cf0e
magda@antix1 <usterijj01@gmail.com>
2023-02-24 08:59:10 -0300

    initial commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,8 +30,8 @@ days_list = [f'Day {x}' for x in md_files]
 
 query_params = st.experimental_get_query_params()
 
-if query_params and query_params["day"][0] in days_list:
-    st.session_state.day = query_params["day"][0]
+if query_params and query_params["challenge"][0] in days_list:
+    st.session_state.day = query_params["challenge"][0]
 
 selected_day = st.selectbox('Start the Challenge 👇', days_list, key="day", on_change=update_params)
 
