c649b94690828f8387dd87c619cf021ac6099ae5
magda@antix1 <usterijj01@gmail.com>
2023-02-27 19:13:03 -0300

    initial commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,8 @@ import glob
 st.set_page_config(
     page_title = "10DaysBalletChallenge",
     page_icon = ":dancers:",
-    initial_sidebar_state="expanded",
+    initial_sidebar_state="collapsed",
+    layout="wide",
 )
 
 def enable_indexability():
